83679b693a838213a5982a436fe815c1f49736be
Timo Stollenwerk <tisto@plone.org>
2015-04-10 16:29:45 +0200

    Pep8. Remove unused imports.

--- src/plone/rest/zcml.py
+++ src/plone/rest/zcml.py
@@ -1,18 +1,11 @@
-from zope import component
-from zope.component.interface import provideInterface
-from zope.component.zcml import handler
+from zope.configuration.exceptions import ConfigurationError
 from zope.configuration.fields import GlobalObject, GlobalInterface
 from zope.interface import Interface
-from zope.publisher.interfaces import IDefaultViewName
-from zope.publisher.interfaces.browser import IBrowserRequest
-from zope.publisher.interfaces.browser import IBrowserSkinType
-from zope.publisher.interfaces.browser import IDefaultSkin
 from zope.schema import TextLine, Bool
 from zope.publisher.interfaces.browser import IBrowserPublisher
 from plone.rest import interfaces
 
 from zope.component.zcml import adapter
-from zope.component.zcml import utility
 
 
 class IService(Interface):
@@ -20,7 +13,8 @@ class IService(Interface):
     """
 
     method = TextLine(
-        title=u"The name of the view that should be the default.[get|post|put|delete]",
+        title=u"The name of the view that should be the default. " +
+              u"[get|post|put|delete]",
         description=u"""
         This name refers to view that should be the view used by
         default (if no view name is supplied explicitly).""",
@@ -36,16 +30,18 @@ class IService(Interface):
 
     factory = GlobalObject(
         title=u"The factory for this behavior",
-        description=u"If this is not given, the behavior is assumed to provide a marker interface")
-
+        description=u"If this is not given, the behavior is assumed to " +
+                    u"provide a marker interface")
 
     layer = GlobalInterface(
         title=u"A marker interface to be applied by the behavior",
-        description=u"If provides is given and factory is not given, then this is optional",
+        description=u"If provides is given and factory is not given, then "
+                    u"this is optional",
         required=False)
 
     cors_enabled = Bool(
-        title=u"The name of the view that should be the default.[get|post|put|delete]",
+        title=u"The name of the view that should be the default."
+              u"[get|post|put|delete]",
         description=u"""
         This name refers to view that should be the view used by
         default (if no view name is supplied explicitly).""",
@@ -53,15 +49,24 @@ class IService(Interface):
         )
 
     cors_origin = TextLine(
-        title=u"The name of the view that should be the default.[get|post|put|delete]",
+        title=u"The name of the view that should be the default." +
+              u"[get|post|put|delete]",
         description=u"""
         This name refers to view that should be the view used by
         default (if no view name is supplied explicitly).""",
         required=False
         )
 
-def serviceDirective(_context, method, factory, for_, layer=None, cors_enabled=False, cors_origin=None):
-    
+
+def serviceDirective(
+        _context,
+        method,
+        factory,
+        for_,
+        layer=None,
+        cors_enabled=False,
+        cors_origin=None):
+
     if method.upper() == 'GET':
         marker = interfaces.IGET
     elif method.upper() == 'POST':
@@ -73,13 +78,15 @@ def serviceDirective(_context, method, factory, for_, layer=None, cors_enabled=F
     elif method.upper() == 'DELETE':
         marker = interfaces.IDELETE
     else:
-        raise ConfigurationError(u"No implementation for %s method" % method)
+        raise ConfigurationError(
+            u"No implementation for %s method" % method
+        )
 
     # Instantiate the real factory if it's the schema-aware type. We do
     # this here so that the for_ interface may take this into account.
     # if factory is not None and ISchemaAwareFactory.providedBy(factory):
     #     factory = factory(provides)
-    
+
     # registration = ServiceRegistration(
     #     title=title,
     #     description=description,
@@ -88,8 +95,10 @@ def serviceDirective(_context, method, factory, for_, layer=None, cors_enabled=F
     #     factory=factory)
 
     # adapter_factory = ServiceAdapterFactory(registration)
-    
-    adapter(_context, 
-            factory=(factory,),
-            provides=IBrowserPublisher,
-            for_=(for_,marker))
+
+    adapter(
+        _context,
+        factory=(factory,),
+        provides=IBrowserPublisher,
+        for_=(for_, marker)
+    )
