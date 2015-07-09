bb9625e5533f5de4872728943f1ba4332546c0c2
Timo Stollenwerk <tisto@plone.org>
2015-07-09 17:46:15 +0200

    Remove unused import.

--- src/plone/rest/testing.py
+++ src/plone/rest/testing.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
 
