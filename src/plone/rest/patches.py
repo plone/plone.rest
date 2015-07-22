37c0a6ebd4455d0c2bf850a360491dfdf340a07f
Timo Stollenwerk <tisto@plone.org>
2015-07-22 17:40:44 +0200

    Revert "Make it work with archetypes on plone 4.3"
    This reverts commit d86f13d0e11697530ff3846db8b9177a9116c423.

--- src/plone/rest/patches.py
+++ src/plone/rest/patches.py
@@ -34,17 +34,3 @@ def DexterityContent__before_publishing_traverse__(self, arg1, arg2=None):
 
         super(DexterityContent, self).__before_publishing_traverse__(
             arg1, arg2)
-
-
-def ArchetypesContent__before_publishing_traverse__(self, arg1, arg2=None):
-        """Pre-traversal hook that stops traversal to prevent the default view
-           to be appended. Appending the default view will break REST calls.
-        """
-        REQUEST = arg2 or arg1
-
-        from plone.rest.interfaces import IAPIRequest
-        if IAPIRequest.providedBy(REQUEST):
-            return
-
-        super(DexterityContent, self).__before_publishing_traverse__(
-            arg1, arg2)
