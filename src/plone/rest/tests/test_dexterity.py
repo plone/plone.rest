70e3b1370976bb34b7e44dd3054713371b1ae2c4
Timo Stollenwerk <tisto@plone.org>
2015-07-23 17:12:55 +0200

    Do a transaction before testing the folder json view.

--- src/plone/rest/tests/test_dexterity.py
+++ src/plone/rest/tests/test_dexterity.py
@@ -7,6 +7,7 @@ from plone.rest.testing import PLONE_REST_FUNCTIONAL_TESTING
 
 import unittest
 import requests
+import transaction
 
 
 class TestDexterityServiceEndpoints(unittest.TestCase):
@@ -21,7 +22,6 @@ class TestDexterityServiceEndpoints(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.portal.invokeFactory('Document', id='doc1')
         self.document = self.portal['doc1']
-        import transaction
         transaction.commit()
 
     def test_dexterity_get(self):
@@ -98,11 +98,14 @@ class TestDexterityServiceEndpoints(unittest.TestCase):
 
     def test_dexterity_folder_get(self):
         self.portal.invokeFactory('Folder', id='folder')
+        transaction.commit()
+
         response = requests.get(
             self.portal.folder.absolute_url(),
             headers={'Accept': 'application/json'},
             auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
         )
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             {u'portal_id': u'plone'},
