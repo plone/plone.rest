e2a045210c71ff9dd4a86c5d9ed5056e4ea7e9a1
Ramon Navarro Bosch <ramon.nb@gmail.com>
2015-07-23 16:27:31 +0200

    Solving problems on before traverse ( event was not triggered )

--- src/plone/rest/patches.py
+++ src/plone/rest/patches.py
@@ -3,6 +3,9 @@ from plone.rest.interfaces import IAPIRequest
 from plone.rest.events import mark_as_api_request
 from plone.dexterity.content import DexterityContent
 from Products.CMFPlone.Portal import PloneSite
+from zope.component.interfaces import ComponentLookupError
+from zope.event import notify
+from zope.traversing.interfaces import BeforeTraverseEvent
 
 
 def PloneSite__before_publishing_traverse__(self, arg1, arg2=None):
@@ -11,11 +14,22 @@ def PloneSite__before_publishing_traverse__(self, arg1, arg2=None):
     """
     REQUEST = arg2 or arg1
 
-    if IAPIRequest.providedBy(REQUEST):
-        return
+    goon = True
 
-    if REQUEST.getHeader('Accept') == 'application/json':
+    if IAPIRequest.providedBy(REQUEST):
+        goon = False
+
+    if goon and REQUEST.getHeader('Accept') == 'application/json':
         mark_as_api_request(REQUEST)
+        goon = False
+
+    if not goon:
+        # Copied of CMFCore PortalObject
+        try:
+            notify(BeforeTraverseEvent(self, REQUEST))
+        except ComponentLookupError:
+            # allow ZMI access, even if the portal's site manager is missing
+            pass
         return
 
     super(PloneSite, self).__before_publishing_traverse__(
@@ -30,8 +44,13 @@ def DexterityContent__before_publishing_traverse__(self, arg1, arg2=None):
 
         from plone.rest.interfaces import IAPIRequest
         if IAPIRequest.providedBy(REQUEST):
+            # Copied of CMFCore PortalObject
+            try:
+                notify(BeforeTraverseEvent(self, REQUEST))
+            except ComponentLookupError:
+                # allow ZMI access, even if the portal's site manager is missing
+                pass
             return
-
         super(DexterityContent, self).__before_publishing_traverse__(
             arg1, arg2)
 
@@ -44,6 +63,12 @@ def ArchetypesContent__before_publishing_traverse__(self, arg1, arg2=None):
 
         from plone.rest.interfaces import IAPIRequest
         if IAPIRequest.providedBy(REQUEST):
+            # Copied of CMFCore PortalObject
+            try:
+                notify(BeforeTraverseEvent(self, REQUEST))
+            except ComponentLookupError:
+                # allow ZMI access, even if the portal's site manager is missing
+                pass
             return
 
         super(DexterityContent, self).__before_publishing_traverse__(
