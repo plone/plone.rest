9f898701dd7dfcbd98245550d56cccd213c84d9b
Franco Pellegrini <frapell@gmail.com>
2018-10-03 15:34:24 +0000

    Do not return an error when exceptions differ, if on a WSGIRequest

--- src/plone/rest/errors.py
+++ src/plone/rest/errors.py
@@ -8,6 +8,11 @@ from six.moves import urllib
 from six.moves.urllib.parse import quote
 from six.moves.urllib.parse import unquote
 from zExceptions import NotFound
+try:
+    from ZPublisher.HTTPRequest import WSGIRequest
+    HAS_WSGI = True
+except:
+    HAS_WSGI = False
 from zope.component import adapter
 from zope.component import queryUtility
 from zope.component.hooks import getSite
@@ -16,9 +21,6 @@ import json
 import six
 import sys
 import traceback
-
-
-PY3 = sys.version_info[0] == 3
 
 
 @adapter(Exception, IAPIRequest)
@@ -73,10 +75,13 @@ class ErrorHandling(BrowserView):
     def render_traceback(self, exception):
         _, exc_obj, exc_traceback = sys.exc_info()
         if exception is not exc_obj:
-            if not PY3:
-                # XXX: Only display this warning if not in Python 3.
+            if HAS_WSGI and isinstance(self.request, WSGIRequest) and str(exception) == str(exc_obj):
+                # WSGIRequest may "upgrade" the exception, resulting in a new exception
+                # which has the same string representation as the original exception.
                 # https://github.com/plone/Products.CMFPlone/issues/2474
                 # https://github.com/plone/plone.rest/commit/96599cc3bb3ef5a23b10eb585781d88274fbcaf5#comments
+                pass
+            else:
                 return (u'ERROR: Another exception happened before we could '
                         u'render the traceback.')
 
