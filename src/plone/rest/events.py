d431f1c0772bdcfc32db33f89f21e40d6cbbd1e8
Jens W. Klein <jk@kleinundpartner.at>
2021-10-05 21:30:44 +0200

    REST traverser ++api++ (#113)
    * Draft proposal of rest traverser
    
    * refactor to reuse marking logic in as non-subscriber
    
    * use ++api++ as traverser
    
    * implement rest marking traverser ++api++
    
    * document changes

--- src/plone/rest/events.py
+++ src/plone/rest/events.py
@@ -5,18 +5,16 @@ from plone.rest.negotiation import lookup_service_id
 from zope.interface import alsoProvides
 
 
-def mark_as_api_request(event):
+def mark_as_api_request(request, accept):
     """Mark a request as IAPIRequest if there's a service registered for the
     actual request method and Accept header.
     """
-    request = event.request
     method = request.get("REQUEST_METHOD", "GET")
     if method == "OPTIONS" and request.getHeader("Origin", False):
         preflighted_method = request.getHeader("Access-Control-Request-Method", None)
         service_id = lookup_preflight_service_id(preflighted_method)
         request._rest_cors_preflight = True
     else:
-        accept = request.getHeader("Accept", "text/html")
         service_id = lookup_service_id(method, accept)
         request._rest_cors_preflight = False
 
@@ -27,3 +25,12 @@ def mark_as_api_request(event):
         # Flag as non-WebDAV request in order to avoid special treatment
         # in ZPublisher.BaseRequest.traverse().
         request.maybe_webdav_client = 0
+
+
+def subscriber_mark_as_api_request(event):
+    """Subscriber to mark a request as IAPIRequest (see mark_as_api_request)
+    """
+    mark_as_api_request(
+        event.request, 
+        event.request.getHeader("Accept", "text/html"),
+    )    
