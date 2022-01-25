55986be8478c38ae7f91d871fa6a3bd89eed5ecc
Timo Stollenwerk <stollenwerk@kitconcept.com>
2022-01-25 12:48:57 +0100

    Back to development: 2.0.0a3
    [ci skip]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "2.0.0a2"
+version = "2.0.0a3.dev0"
 
 long_description = read("README.rst") + "\n\n" + read("CHANGES.rst") + "\n\n"
 
