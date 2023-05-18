cbe58a3cc2da4894e8657c637f998ac4c5606cde
Roel Bruggink <roel@jaroel.nl>
2023-05-18 11:23:19 +0200

    Renamed IExplicitAcquisitionPublishingAllowed to IShouldAllowAcquiredItemPublication

--- src/plone/rest/explicitacquisition.py
+++ src/plone/rest/explicitacquisition.py
@@ -1,8 +1,8 @@
 from zope.component import adapter
-from Products.CMFCore.interfaces import IExplicitAcquisitionPublishingAllowed
+from Products.CMFCore.interfaces import IShouldAllowAcquiredItemPublication
 from plone.rest.traverse import RESTWrapper
 
 
 @adapter(RESTWrapper)
 def rest_allowed(wrapper):
-    return IExplicitAcquisitionPublishingAllowed(wrapper.context)
+    return IShouldAllowAcquiredItemPublication(wrapper.context)
