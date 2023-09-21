1740f6f05ec0a5f466716ac35b6648f950045ca1
Timo Stollenwerk <stollenwerk@kitconcept.com>
2023-09-21 20:39:58 +0200

    Don't break on py 3.6 and 3.7, just drop official support

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     extras_require=dict(
         test=[
             "plone.app.testing[robot]>=4.2.2",
