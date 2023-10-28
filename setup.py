10b34dfa022dd6572e245ab0b56f0666ff212f42
Timo Stollenwerk <stollenwerk@kitconcept.com>
2023-10-28 22:14:05 +0200

    Back to development: 4.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "4.1.2"
+version = "4.1.3.dev0"
 
 long_description = read("README.rst") + "\n\n" + read("CHANGES.rst") + "\n\n"
 
