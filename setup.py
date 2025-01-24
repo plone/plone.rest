3a0142c8618681af6a2c21e43f678333eea8af39
David Glick <david@glicksoftware.com>
2025-01-24 11:14:46 -0800

    fix CI

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
