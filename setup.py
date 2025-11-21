317cb33f647f1fdc7492308b7cbff031e42e7d8d
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-11-21 23:19:12 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "5.1.1.dev0"
+version = "6.0.0.dev0"
 
 long_description = read("README.rst") + "\n\n" + read("CHANGES.rst") + "\n\n"
 
